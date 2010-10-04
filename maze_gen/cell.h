b9e363097f76d0598082c62ce7587f166b056bd2
Ausbury <ausburyl@eos22.cis.gvsu.edu>
2010-10-03 22:43:03 -0400

    initializes maze to all zeroes, setoes, sets up neighbors, and begins algorithm.  Not finished.

--- maze_gen/cell.h
+++ maze_gen/cell.h
@@ -1,17 +1,14 @@
 
-struct cell;
+struct cell {
+	int state;
+	int northWall;
+	int southWall;
+	int westWall;
+	int eastWall;
+	struct cell * north;
+	struct cell * south;
+	struct cell * east;
+	struct cell * west;
+};
 
-typedef struct cell
-{
-int state;
-int northWall;
-int southWall;
-int westWall;
-int eastWall;
-struct cell *north;
-struct cell *south;
-struct cell *east;
-struct cell *west;
-} Cell;
-
-
+typedef struct cell Cell;
