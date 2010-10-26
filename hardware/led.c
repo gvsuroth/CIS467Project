b8fea31eec83b0cec9e1aa39c7002ed19cb5ab7a
Joseph Roth <rothjo@mail.gvsu.edu>
2010-10-26 10:21:31 -0400

    Rotate led lights to simulate always on.

--- hardware/led.c
+++ hardware/led.c
@@ -9,7 +9,21 @@ int main() {
 		printf("Could not connect to the port.\n"), exit(1);
 
 	// 255 sets all ports to high
-	outb(255, port);
-	sleep(2);
+  int disp[5];
+  disp[0] = 1;
+  disp[1] = 0;
+  disp[2] = 0;
+  disp[3] = 1;
+  disp[4] = 1;
+
+  int step = 0;
+  while(1) {
+    int out = disp[step] << step;
+    outb(out, port);
+    step++;
+    usleep(5000);
+    printf("Out: %d\n", out);
+  }
+
 	return 0;
 }
